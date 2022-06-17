a39b14288ffc2472266666f4cbdee383a04aef42
Docinhoh <87666816+Docinhoh@users.noreply.github.com>
2022-06-17 18:51:50 -0300

    update

--- script.js
+++ script.js
@@ -1,77 +1,27 @@
-// Painel produtos
+//BOTAO WTS
 
-const btnhid = document.querySelector('#btnhid');
-const pnlhid = document.querySelector('#pnlhid');
-
-btnhid.addEventListener('click', function(){
-    pnlhid.classList.toggle('hid');
-    intvida.classList.add('hid');
-    intsaude.classList.add('hid');
-    intfinan.classList.add('hid');
-})
-
-
-//painel produtos interno
-
-const vida = document.querySelector('#vida')
-const saude = document.querySelector('#saude')
-const finan = document.querySelector('#finan')
-
-const intvida = document.querySelector('#intvida')
-const intsaude = document.querySelector('#intsaude')
-const intfinan = document.querySelector('#intfinan')
-
-const intvida1 = document.querySelector('#intvida1')
-const intsaude1 = document.querySelector('#intsaude1')
-const intfinan1 = document.querySelector('#intfinan1')
-
-vida.addEventListener('click', function(){
-    intvida.classList.toggle('hid');
-    intsaude.classList.add('hid');
-    intfinan.classList.add('hid');
-
-    intvida1.classList.toggle('hid');
-    intsaude1.classList.add('hid');
-    intfinan1.classList.add('hid');
-
-
-
-})
-
-saude.addEventListener('click', function(){
-    intvida.classList.add('hid');
-    intsaude.classList.toggle('hid');
-    intfinan.classList.add('hid');
-
-    intvida1.classList.add('hid');
-    intsaude1.classList.toggle('hid');
-    intfinan1.classList.add('hid');
-})
-
-finan.addEventListener('click', function(){
-    intvida.classList.add('hid');
-    intsaude.classList.add('hid');
-    intfinan.classList.toggle('hid');
-
-    intvida1.classList.add('hid');
-    intsaude1.classList.add('hid');
-    intfinan1.classList.toggle('hid');
-
-})
-
-//whats popup
-
-const h4 = document.querySelectorAll('.btnwts');
 const url = "https://wa.me/5541987299547?text=Olá,%20vim%20do%20site%20da%20GNSIX%20e%20gostaria%20de%20saber%20mais."
+const urlv = "https://wa.me/5541987299547?text=Olá,%20vim%20do%20site%20da%20GNSIX%20e%20gostaria%20de%20saber%20mais%20sobre%20Seguro%20de%20Vida."
+const urls = "https://wa.me/5541987299547?text=Olá,%20vim%20do%20site%20da%20GNSIX%20e%20gostaria%20de%20saber%20mais%20sobre%20Plano%20de%20Saúde."
+const urlf = "https://wa.me/5541987299547?text=Olá,%20vim%20do%20site%20da%20GNSIX%20e%20gostaria%20de%20saber%20mais%20sobre%20Planejamento%20Financeiro."
 
 function openInNewTab(url){
     const win = window.open(url, '_blank');
     win.focus();
 }
-
-for (let i = 0; i < h4.length; i++){
-    h4[i].addEventListener('click', function(){
-        openInNewTab(url);
-    })
+function openInNewTabv(urlv){
+    const win = window.open(urlv, '_blank');
+    win.focus();
 }
+function openInNewTabs(urls){
+    const win = window.open(urls, '_blank');
+    win.focus();
+}
+function openInNewTabf(urlf){
+    const win = window.open(urlf, '_blank');
+    win.focus();
+}
+
+
+// MEDAI QEURY
 
